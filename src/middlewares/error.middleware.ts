f43f9c8eaff76474cc0917669f3da9bff263cbff
Lucas Duarte <lucs.rkt@gmail.com>
2022-03-29 19:54:30 -0300

    feat(middlewares/ErrorMiddleware): created

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+import ErrorMessage from '../enums/ErrorMessage';
+import HttpStatusCode from '../enums/HttpStatusCode';
+
+export default (err: unknown, req: Request, res:Response, _next: NextFunction) => {
+  console.log(err);
+
+  return res.status(HttpStatusCode.SERVER_ERROR)
+    .json(ErrorMessage.SERVER_ERROR);
+};
